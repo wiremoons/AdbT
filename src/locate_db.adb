2131d0ed61cf9a9ce7fa02b16be5df5c4abce10c
Simon Rowe <simon@wiremoons.com>
2020-12-06 12:20:41 +0000

    Formating changes by gnatpp

--- src/locate_db.adb
+++ src/locate_db.adb
@@ -12,15 +12,18 @@ with Ada.Directories; use Ada.Directories;
 package body Locate_DB is
 
    -----------------------------------------------
-   --  Locate a DataBase file
+   --  Locate a Database file
    -----------------------------------------------
    function Get_DB_Filename
      (dbfile : in out Unbounded_String) return Boolean is
 
-   -- Try to locate the database file name and path and return it as and
-   -- Unbounded_String if found. Boolean is returned bases on success.
+   -- Try to locate the database file name and path and update the passed in
+   -- Unbounded_String if found. Boolean is returned based the success of
+   -- finding the dbfile or not.
+
    begin
-      -- check environment varialble "ACRODB" for a database file
+
+      -- check environment variable "ACRODB" for a database file
       if Ada.Environment_Variables.Exists ("ACRODB") then
          dbfile :=
            (To_Unbounded_String
@@ -29,39 +32,37 @@ package body Locate_DB is
            (Put_Line
               (Standard_Error,
                "DEBUG: environment variable 'ACRODB' is: '" &
-               To_String (dbfile) & "'"));
-         -- check the database file actually exists as well...
+                 To_String (dbfile) & "'"));
+
          if Exists (To_String (dbfile)) then
+            -- environment path for database file exists - return it
             return True;
          end if;
+
          Put (Standard_Error, "ERROR: 'ACRODB' env file location given as: '");
          Put (Standard_Error, To_String (dbfile) & "' - ");
          Put_Line (Standard_Error, "but file location not found.");
       end if;
 
-      -- check same directory as program executed in for filename 'acronyms.db'
+      -- check for filename 'acronyms.db' in the same directory as the program
       dbfile :=
         (To_Unbounded_String
            (Containing_Directory (Ada.Command_Line.Command_Name)));
-      pragma Debug
-        (Put_Line
-           (Standard_Error,
-            "DEBUG: Containing directory: '" & To_String (dbfile) & "'"));
       dbfile := dbfile & "/acronyms.db";
       pragma Debug
         (Put_Line
            (Standard_Error,
-            "DEBUG: Contructed dbfile is: '" & To_String (dbfile) & "'"));
-      -- check of contructed database filename exists?
+            "DEBUG: programs local 'dbfile' path is: '" & To_String (dbfile) & "'"));
+
       if Exists (To_String (dbfile)) then
          -- constructed filename for database file exists - return it
          return True;
       else
-         -- reset 'dbfile' variable as contructed file name does not exist
+         -- reset 'dbfile' variable as constructed file name does not exist
          dbfile := Null_Unbounded_String;
          pragma Debug
            (Put_Line
-              (Standard_Error, "DEBUG: No constructed database file found"));
+              (Standard_Error, "DEBUG: No programs local database file found"));
          return False;
       end if;
    end Get_DB_Filename;
