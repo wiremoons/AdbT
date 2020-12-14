87d35e4a12135c6c9939ff7a04726358b7cfa1d8
Simon Rowe <simon@wiremoons.com>
2020-12-14 07:01:35 +0000

    Reformating wiht gnatpp

--- src/locate_db.adb
+++ src/locate_db.adb
@@ -17,13 +17,13 @@ package body Locate_DB is
    function Get_DB_Filename
      (dbfile : in out Unbounded_String) return Boolean is
 
-   -- Try to locate the database file name and path and update the passed in
-   -- Unbounded_String if found. Boolean is returned based the success of
-   -- finding the dbfile or not.
+   --  Try to locate the database file name and update the passed in
+   --  Unbounded_String if found. Boolean is returned based the success
+   --  of finding the dbfile or not.
 
    begin
 
-      -- check environment variable "ACRODB" for a database file
+      --  check environment variable "ACRODB" for a database file
       if Ada.Environment_Variables.Exists ("ACRODB") then
          dbfile :=
            (To_Unbounded_String
@@ -32,10 +32,10 @@ package body Locate_DB is
            (Put_Line
               (Standard_Error,
                "DEBUG: environment variable 'ACRODB' is: '" &
-                 To_String (dbfile) & "'"));
+               To_String (dbfile) & "'"));
 
          if Exists (To_String (dbfile)) then
-            -- environment path for database file exists - return it
+            --  environment path for database file exists - return it
             return True;
          end if;
 
@@ -44,7 +44,7 @@ package body Locate_DB is
          Put_Line (Standard_Error, "but file location not found.");
       end if;
 
-      -- check for filename 'acronyms.db' in the same directory as the program
+      --  check for filename 'acronyms.db' in the same directory as the program
       dbfile :=
         (To_Unbounded_String
            (Containing_Directory (Ada.Command_Line.Command_Name)));
@@ -52,17 +52,19 @@ package body Locate_DB is
       pragma Debug
         (Put_Line
            (Standard_Error,
-            "DEBUG: programs local 'dbfile' path is: '" & To_String (dbfile) & "'"));
+            "DEBUG: programs local 'dbfile' path is: '" & To_String (dbfile) &
+            "'"));
 
       if Exists (To_String (dbfile)) then
-         -- constructed filename for database file exists - return it
+         --  constructed filename for database file exists - return it
          return True;
       else
-         -- reset 'dbfile' variable as constructed file name does not exist
+         --  reset 'dbfile' variable as constructed file name does not exist
          dbfile := Null_Unbounded_String;
          pragma Debug
            (Put_Line
-              (Standard_Error, "DEBUG: No programs local database file found"));
+              (Standard_Error,
+               "DEBUG: No programs local database file found"));
          return False;
       end if;
    end Get_DB_Filename;
