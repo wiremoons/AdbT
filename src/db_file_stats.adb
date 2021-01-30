9f2733542a1059c70aede6261cc988499a12f4cd
Simon Rowe <simon@wiremoons.com>
2021-01-30 14:09:22 +0000

    Add missing exception handling

--- src/db_file_stats.adb
+++ src/db_file_stats.adb
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- Package     : DB_File_Stats                                                   --
+-- Package     : DB_File_Stats                                               --
 -- Description : Find the acronyms database file for use with the program.   --
 -- Author      : Simon Rowe <simon@wiremoons.com>                            --
 -- License     : MIT Open Source.                                            --
@@ -7,6 +7,7 @@
 with Ada.Directories; use Ada.Directories;
 with Ada.Text_IO;     use Ada.Text_IO;
 -- with Text_IO.Editing;
+with Ada.Strings.Fixed;
 with Ada.IO_Exceptions;
 with Ada.Calendar.Formatting; use Ada.Calendar.Formatting;
 
@@ -24,7 +25,26 @@ package body DB_File_Stats is
       --
       --  TODO : add thousands coma formating
       Dbfile_Size := (Size (Dbfile));
-      return (Dbfile_Size'Image);
+      return (Ada.Strings.Fixed.Trim (Dbfile_Size'Image, Ada.Strings.Left));
+
+   exception
+      when Ada.Directories.Name_Error =>
+         --  error with provided file name
+         New_Line (2);
+         Put
+           (Standard_Error,
+            "ERROR: DB_File_Stats.Get_File_Size() NAME for: '");
+         Put_Line (Standard_Error, Dbfile & "'.");
+         return ("ERROR");
+      when Constraint_Error =>
+         --  error with provided file name
+         New_Line (2);
+         Put
+           (Standard_Error,
+            "ERROR: DB_File_Stats.Get_File_Size() SIZE for: '");
+         Put_Line (Standard_Error, Dbfile & "'.");
+         return ("ERROR");
+
    end Get_File_Size;
 
    function Get_Full_Directory (Dbfile : String) return String is
